9ff8bbf727de3488b16dbfee34e8d7c26c313941
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-11-03 22:40:40 -0600

    Add multipage support

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 import htmlPurge from 'vite-plugin-purgecss';
 
-export default {
+export default defineConfig({
+  root: resolve(__dirname, 'src/'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/pages/about/index.html'),
+        contact: resolve(__dirname, 'src/pages/contact/index.html'),
+      },
+    },
+  },
   plugins: [htmlPurge()],
-};
+});
